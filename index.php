1fb76c48f33e93f62ccbad8c3700c607b18851b9
zaldoiii <43696167+zaldoiii@users.noreply.github.com>
2019-04-20 17:05:37 +0700

    Update index.php

--- index.php
+++ index.php
@@ -45,7 +45,7 @@ $app->post('/', function ($request, $response)
 			if ($event['message']['type'] == 'text')
 			{
 				$inputMessage = 'python ccd.py ' . $event['message']['text'];
-				$outputMessage = new TextMessageBuilder(shell_exec($inputMessage));
+				$outputMessage = new TextMessageBuilder(exec($inputMessage));
 				
 				$result = $bot->replyMessage($event['replyToken'], $outputMessage);
 				return $result->getHTTPStatus() . ' ' . $result->getRawBody();
