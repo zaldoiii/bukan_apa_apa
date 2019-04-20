bd27c175e22ab0a34866f3042b6063fb3340d7b5
zaldoiii <43696167+zaldoiii@users.noreply.github.com>
2019-04-20 15:26:25 +0700

    Update index.php

--- index.php
+++ index.php
@@ -44,8 +44,8 @@ $app->post('/', function ($request, $response)
 		{
 			if ($event['message']['type'] == 'text')
 			{
-				$inputMessage = 'python ccd.py ' . $event['message']['text'];
-				$outputMessage = new TextMessageBuilder("Mantap bujang!");
+				$inputMessage = 'python BM.py ' . $event['message']['text'];
+				$outputMessage = new TextMessageBuilder(shell_exec($inputMessage));
 				
 				$result = $bot->replyMessage($event['replyToken'], $outputMessage);
 				return $result->getHTTPStatus() . ' ' . $result->getRawBody();
